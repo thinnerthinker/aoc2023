18d5389953be930b7cb579db1f6fec5e7e18128c
Thinner Thinker <thinnrthinkr@gmail.com>
2023-12-04 15:55:53 +0200

    adjust solver type for arbitrary num of parts

--- test/aoc2023.ml
+++ test/aoc2023.ml
@@ -1,19 +1,21 @@
 open Aoc
 
-type solver = int * ((string list -> int) * (string list -> int)) 
+type solver = int * (string list -> int) list
 
 let () = 
   (* added explicit day indices so i can comment the previous days out while debugging *)
   let solvers : solver list = [ 
-    1, (Day01.part1, Day01.part2);
-    2, (Day02.part1, Day02.part2);
-    3, (Day03.part1, Day03.part2);
-    4, (Day04.part1, Day04.part2);
+    1, [Day01.part1; Day01.part2];
+    2, [Day02.part1; Day02.part2];
+    3, [Day03.part1; Day03.part2];
+    4, [Day04.part1; Day04.part2];
   ] in
   
-  let _ = solvers |> List.map (fun (i, (p1, p2)) -> 
+  let _ = solvers |> List.map (fun (i, ps) -> 
     let ind = string_of_int i in
     let input = Util.read_all_lines ("inputs/input" ^ ind ^ ".txt") in
-    print_endline ("Day " ^ ind ^ ": " ^ string_of_int (p1 input) ^ " " ^ string_of_int (p2 input)))
+    print_endline ("Day " ^ ind ^ ": " ^ 
+      (List.map (fun p -> string_of_int (p input)) ps |> String.concat " ")
+    ))
   in
   ()
