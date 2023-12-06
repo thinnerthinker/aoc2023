510c44023eb300c7498838084da862215a17b6d8
Thinner Thinker <thinnrthinkr@gmail.com>
2023-12-06 16:26:53 +0200

    add day6 part2

--- bin/day06.ml
+++ bin/day06.ml
@@ -3,14 +3,14 @@ open Util
 let is_blank x = String.length x == 0
 let numbers_of_string = String.split_on_char ' ' >> List.filter (is_blank >> not) >> List.map int_of_string
 
-let numbers_of_line line = 
-  List.nth (String.split_on_char ':' line) 1 |> numbers_of_string 
+let numbers_of_line line = List.nth (String.split_on_char ':' line) 1 |> numbers_of_string 
 
 let hard_floor x = (if Float.is_integer x then (Float.sub x 1.) else Float.floor x) |> int_of_float
 let hard_ceil x = (if Float.is_integer x then (Float.add x 1.) else Float.ceil x) |> int_of_float
 
-let part1 input = 
-  let numbers = input |> List.map numbers_of_line 
+
+let solve build_numbers input = 
+  let numbers = input |> List.map (numbers_of_line >> build_numbers)
   in
   List.map2 (fun t d -> (
     let delta = sqrt (float_of_int (t * t - 4 * d)) in
@@ -19,3 +19,6 @@ let part1 input =
     right - left + 1
   )) (List.nth numbers 0) (List.nth numbers 1)
   |> List.fold_left Int.mul 1
+
+let part1 = solve Fun.id
+let part2 = solve (fun ns -> [List.fold_left (fun a n -> a ^ string_of_int n) "" ns |> int_of_string])
