4b571914b04c2010f0e1c50a051cc1bff2b284df
Thinner Thinker <thinnrthinkr@gmail.com>
2023-12-06 16:31:36 +0200

    figure out float version of binary ops exist

--- bin/day06.ml
+++ bin/day06.ml
@@ -5,8 +5,8 @@ let numbers_of_string = String.split_on_char ' ' >> List.filter (is_blank >> not
 
 let numbers_of_line line = List.nth (String.split_on_char ':' line) 1 |> numbers_of_string 
 
-let hard_floor x = (if Float.is_integer x then (Float.sub x 1.) else Float.floor x) |> int_of_float
-let hard_ceil x = (if Float.is_integer x then (Float.add x 1.) else Float.ceil x) |> int_of_float
+let hard_floor x = (if Float.is_integer x then x +. 1. else Float.floor x) |> int_of_float
+let hard_ceil x = (if Float.is_integer x then x +. 1. else Float.ceil x) |> int_of_float
 
 
 let solve build_numbers input = 
@@ -14,9 +14,7 @@ let solve build_numbers input =
   in
   List.map2 (fun t d -> (
     let delta = sqrt (float_of_int (t * t - 4 * d)) in
-    let left = hard_ceil (Float.div (Float.sub (float_of_int t) delta) 2.) in
-    let right = hard_floor (Float.div (Float.add (float_of_int t) delta) 2.) in
-    right - left + 1
+    hard_ceil ((float_of_int t -. delta) /. 2.) - hard_floor ((float_of_int t +. delta) /. 2.) + 1
   )) (List.nth numbers 0) (List.nth numbers 1)
   |> List.fold_left Int.mul 1
 
