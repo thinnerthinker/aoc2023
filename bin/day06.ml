027f17b296429162e2527371beff501340059bbd
Thinner Thinker <thinnrthinkr@gmail.com>
2023-12-06 16:39:55 +0200

    get rid of custom rounding with smarter math

--- bin/day06.ml
+++ bin/day06.ml
@@ -5,16 +5,12 @@ let numbers_of_string = String.split_on_char ' ' >> List.filter (is_blank >> not
 
 let numbers_of_line line = List.nth (String.split_on_char ':' line) 1 |> numbers_of_string 
 
-let hard_floor x = (if Float.is_integer x then x +. 1. else Float.floor x) |> int_of_float
-let hard_ceil x = (if Float.is_integer x then x +. 1. else Float.ceil x) |> int_of_float
-
-
 let solve build_numbers input = 
   let numbers = input |> List.map (numbers_of_line >> build_numbers)
   in
   List.map2 (fun t d -> (
     let delta = sqrt (float_of_int (t * t - 4 * d)) in
-    hard_ceil ((float_of_int t -. delta) /. 2.) - hard_floor ((float_of_int t +. delta) /. 2.) + 1
+    int_of_float (ceil ((float_of_int t +. delta) /. 2. -. 1.) -. floor ((float_of_int t -. delta) /. 2. +. 1.)) + 1
   )) (List.nth numbers 0) (List.nth numbers 1)
   |> List.fold_left Int.mul 1
 
