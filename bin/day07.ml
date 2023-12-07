1c2ce69e109979f2126e5deb804dd152e3781ec5
Thinner Thinker <thinnrthinkr@gmail.com>
2023-12-07 20:31:37 +0200

    add day7 part2

--- bin/day07.ml
+++ bin/day07.ml
@@ -2,19 +2,44 @@ open Aoc2023.Util
 open Aoc2023.Parsing
 
 let id_of_suit s = match s with '0'..'9' -> s | 'T' -> 'A' | 'J' -> 'B' | 'Q' -> 'C' | 'K' -> 'D' | 'A' -> 'E' | _ -> raise Unreachable
-let type_of_card = List.sort Char.compare 
+
+let groups_of_hand = List.sort Char.compare 
 >> List.to_seq >> Seq.group Char.equal >> Seq.map Seq.length 
 >> List.of_seq >> List.sort Int.compare >> List.rev
->> function [5] -> 'Z' | [4; 1] -> 'Y' | [3; 2] -> 'X' | [3; 1; 1] -> 'W' | [2; 2; 1] -> 'V' | [2; 1; 1; 1] -> 'U' | _ -> 'T'
 
-let parse_input = p_any (p_new (fun t s -> t, s)
-|= p_ifs c_alnum +> List.map id_of_suit +> (fun c -> type_of_card c :: c) +> string_of_char_list 
+let strength_of_groups = function [5] -> 'Z' | [4; 1] -> 'Y' | [3; 2] -> 'X' | [3; 1; 1] -> 'W' | [2; 2; 1] -> 'V' | [2; 1; 1; 1] -> 'U' | _ -> 'T'
+
+let parse_input transform = p_any (p_new (fun t s -> t, s)
+|= p_ifs c_alnum +> transform +> string_of_char_list 
 |. p_sps
 |= p_int |. p_lf_opt)
 
+let joker_up j sc = match j with
+| 0 -> sc
+| 1 -> (match sc with
+  | [4] -> [5]
+  | [3; 1] -> [4; 1]
+  | [2; 2] -> [3; 2]
+  | [2; 1; 1] -> [3; 1; 1]
+  | [1; 1; 1; 1] | _ -> [2; 1; 1; 1])
+| 2 -> (match sc with
+  | [3] -> [5]
+  | [2; 1] -> [4; 1]
+  | [1; 1; 1] | _ -> [3; 1; 1])
+| 3 -> (match sc with
+  | [2] -> [5]
+  | [1; 1] | _ -> [4; 1])
+| 4 | 5 | _ -> [5]
 
-let part1 input = input 
-|> parse_string parse_input
-|> List.sort (fun a b -> String.compare (fst a) (fst b))
-|> List.mapi (fun i (_, s) -> (i + 1) * s)
-|> sum
+
+let solve transform = parse_string (parse_input transform)
+>> List.sort (fun a b -> String.compare (fst a) (fst b))
+>> List.mapi (fun i (_, s) -> (i + 1) * s)
+>> sum
+
+let part1 = solve ((List.map id_of_suit) >> (fun h -> (h |> groups_of_hand |> strength_of_groups) :: h))
+let part2 = solve (fun hand -> 
+  let hand = hand |> List.map (fun c -> if c == 'J' then '0' else c) |> List.map id_of_suit in
+  let no_jokers = hand |> List.filter (Char.equal '0' >> not) in
+  let strength = no_jokers |> groups_of_hand |> joker_up (List.length hand - List.length no_jokers) |> strength_of_groups in
+  strength :: hand)
