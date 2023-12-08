dc4fe27bdab0cc8602af3b701e58f5ce7e7c8230
Thinner Thinker <thinnrthinkr@gmail.com>
2023-12-08 19:45:45 +0200

    add day8 part1

--- test/aoc2023.ml
+++ test/aoc2023.ml
@@ -19,6 +19,7 @@ let () =
     5, [Day05.part1; Day05.part2];
     6, [Day06.part1; Day06.part2];
     7, [Day07.part1; Day07.part2];
+    8, [Day08.part1; ];
   ] in
   
   let _ = solvers |> List.map (fun (i, ps) -> 
