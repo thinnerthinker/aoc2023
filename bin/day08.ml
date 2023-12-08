4cd339f57e70d42056d33ae1341b5181114e0a46
Thinner Thinker <thinnrthinkr@gmail.com>
2023-12-08 20:02:15 +0200

    port part1 to seq

--- bin/day08.ml
+++ bin/day08.ml
@@ -1,11 +1,11 @@
 open Aoc2023.Util
 open Aoc2023.Parsing
 
-type node = { left: string; right: string }
+type node = { label: string; left: string; right: string }
 
 let parse_input = p_new (fun ds ns -> ds, ns)
 |= p_end_lf |. p_lf
-|= p_any (p_new (fun l' l r -> l', { left = l; right = r })
+|= p_any (p_new (fun l' l r -> l', { label = l'; left = l; right = r })
   |= p_ifs c_alnum +> string_of_char_list
   |. p_sps |. p_str "=" |. p_sps |. p_str "("
   |= p_ifs c_alnum +> string_of_char_list
@@ -14,24 +14,14 @@ let parse_input = p_new (fun ds ns -> ds, ns)
   |. p_str ")" |. p_lf_opt
 ) +> List.to_seq +> Hashtbl.of_seq
 
-let take_step map from =
-  let get label = if String.equal label "ZZZ" then None 
-    else Some (Hashtbl.find map label) 
-  in
-  function
-  | 'L' -> get from.left
-  | 'R' -> get from.right
-  | _ -> raise Unreachable
-
-let walk_len map dirs =
-  let rec walk from dirs = match Seq.uncons dirs with
-  | Some (d, ds) -> (match take_step map from d with
-    | Some node -> 1 + walk node ds 
-    | None -> 1)
-  | None -> raise Unreachable
-  in
-  walk (Hashtbl.find map "AAA") dirs
+let take_step map from = function
+| 'L' -> Hashtbl.find map from.left
+| 'R' -> Hashtbl.find map from.right
+| _ -> raise Unreachable
 
 let part1 input = 
   let directions, map = input |> parse_string parse_input in
-  walk_len map (directions |> List.to_seq |> Seq.cycle)
+  directions |> List.to_seq |> Seq.cycle 
+  |> Seq.scan (take_step map) (Hashtbl.find map "AAA")
+  |> Seq.take_while (fun n -> String.equal "ZZZ" n.label |> not)
+  |> Seq.length
