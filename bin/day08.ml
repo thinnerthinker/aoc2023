3b6259460ac8992fe3e50b029b55660857add028
Thinner Thinker <thinnrthinkr@gmail.com>
2023-12-08 23:07:27 +0200

    add day8 part2

--- bin/day08.ml
+++ bin/day08.ml
@@ -19,9 +19,32 @@ let take_step map from = function
 | 'R' -> Hashtbl.find map from.right
 | _ -> raise Unreachable
 
+let uncycle eq seq =
+  let first, rest = Seq.uncons seq |> Option.get in
+  Seq.take_while (eq first >> not) rest |> Seq.cons first
+
+let lcm a b =
+  let rec gcd a b =
+    if b = 0 then a else gcd b (a mod b)
+  in
+  a * b / gcd a b
+
+
 let part1 input = 
   let directions, map = input |> parse_string parse_input in
   directions |> List.to_seq |> Seq.cycle 
   |> Seq.scan (take_step map) (Hashtbl.find map "AAA")
   |> Seq.take_while (fun n -> String.equal "ZZZ" n.label |> not)
   |> Seq.length
+
+let part2 input = 
+  let directions, map = input |> parse_string parse_input in
+  Hashtbl.to_seq_keys map |> Seq.filter (fun l -> l.[2] == 'A')
+  |> Seq.map (fun s ->
+    directions |> List.to_seq |> Seq.cycle 
+    |> Seq.scan (fun (i, _d, from) d -> i + 1, d, take_step map from d) (0, 'L', Hashtbl.find map s)
+    |> Seq.filter (fun (_, _, n) -> n.label.[2] == 'Z')
+    |> uncycle (fun (_, d1, n1) (_, d2, n2) -> d1 == d2 && String.equal n1.label n2.label)
+    |> Seq.map (fun (i, _, _) -> i))
+  |> Seq.fold_left (Seq.map_product lcm) ([1] |> List.to_seq)
+  |> Seq.fold_left min Int.max_int
