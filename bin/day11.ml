4c751c49549411f7fbb028203078e7fb94ff2f56
Thinner Thinker <thinnrthinkr@gmail.com>
2023-12-11 15:15:52 +0200

    add day11 part2

--- bin/day11.ml
+++ bin/day11.ml
@@ -6,11 +6,11 @@ let parse = String.split_on_char '\n' >> List.mapi (fun y -> char_list_of_string
   >> List.filter_map Fun.id)
 >> List.flatten
 
-let expand property make_new l = 
+let expand_by factor property make_new l = 
   let rec exp acc = function
   | x1 :: x2 :: xs -> 
     let d = (x2 - x1) in
-    let e = acc + if d <= 1 then 0 else d - 1 in
+    let e = acc + if d <= 1 then 0 else (d - 1) * (factor - 1) in
     e :: exp e (x2 :: xs)
   | _ -> []
   in
@@ -18,11 +18,14 @@ let expand property make_new l =
   List.map2 (fun e x -> make_new (e + property x) x) (0 :: exp 0 (List.map property l)) l
 
 
-let part1 input = 
+let solve factor input = 
   let galaxies = parse input 
-  |> expand fst (fun x p -> x, snd p)
-  |> expand snd (fun y p -> fst p, y)
+  |> expand_by factor fst (fun x p -> x, snd p)
+  |> expand_by factor snd (fun y p -> fst p, y)
   |> List.to_seq 
   in
   (Seq.map_product (fun (x1, y1) (x2, y2) -> abs (x2 - x1) + abs (y2 - y1)) galaxies galaxies
   |> Seq.fold_left (+) 0) / 2
+
+let part1 = solve 2
+let part2 = solve 1_000_000
